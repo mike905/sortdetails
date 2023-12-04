740466abc672a46d382e5e93e0920193a583bcf3
Mike <522015@mail.ru>
2023-12-04 17:33:04 +0200

    readme

--- start.bat
+++ start.bat
@@ -1,36 +1,32 @@
 @echo off
-echo Настройка и запуск проекта sortdetails...
+SET projectFolder=%userprofile%\Desktop\sortdetails
 
-:: Проверка наличия Git
+echo Проверка наличия Git и Python...
 where git >nul 2>&1
 if %errorlevel% neq 0 (
-    echo Git не найден. Пожалуйста, скачайте и установите Git с https://git-scm.com/downloads и запустите этот скрипт снова.
+    echo Git не обнаружен. Пожалуйста, скачайте и установите Git с https://git-scm.com/downloads
     exit /b
 )
 
-:: Проверка наличия Python
 where python >nul 2>&1
 if %errorlevel% neq 0 (
-    echo Python не найден. Пожалуйста, скачайте и установите Python с https://www.python.org/downloads/ и запустите этот скрипт снова.
+    echo Python не обнаружен. Пожалуйста, скачайте и установите Python с https://www.python.org/downloads/
     exit /b
 )
 
-:: Клонирование репозитория
-echo Клонирование репозитория...
-git clone https://github.com/mike905/sortdetails.git
-cd sortdetails
+echo Клонирование репозитория на рабочий стол...
+git clone https://github.com/mike905/sortdetails.git "%projectFolder%"
+cd "%projectFolder%"
 
-:: Создание виртуального окружения
 echo Создание виртуального окружения...
 python -m venv venv
 call venv\Scripts\activate.bat
 
-:: Установка зависимостей
 echo Установка зависимостей...
 python -m pip install --upgrade pip
 python -m pip install -r requirements.txt
 
 echo Проект готов к запуску.
-echo Для запуска используйте 'python ваш_скрипт.py' внутри виртуального окружения.
+echo Для запуска вашего скрипта, используйте команду: python имя_вашего_скрипта.py
 pause
 
