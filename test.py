ca800a6a7d7b26e0cf08480b81d9112e99ed1ca3
Mike <522015@mail.ru>
2023-12-04 16:57:24 +0200

    first commit

---
+++ test.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+def print_excel_data(filename):
+    try:
+        # Чтение всего файла
+        data = pd.read_excel(filename)
+        print(data)
+    except Exception as e:
+        print(f"Произошла ошибка при чтении файла {filename}: {e}")
+
+# Использование функции
+#print_excel_data("ИЗПА.5177.xlsx")
+
+print_excel_data("ИЗПА.2322.02.xlsx")
+
